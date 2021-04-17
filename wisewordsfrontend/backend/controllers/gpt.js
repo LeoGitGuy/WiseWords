57d6ad6e0facbe467a7df2bea18b8c88e46bd1f3
SimonGoldhofer <simon.goldhofer@gmail.com>
2021-04-17 14:14:11 +0200

    prompts added

--- wisewordsfrontend/backend/controllers/gpt.js
+++ wisewordsfrontend/backend/controllers/gpt.js
@@ -2,14 +2,10 @@ const got = require('got');
 
 const API_KEY = "sk-RfecCUXHspo3wLUqT5OxVRZUiveGmVsIAYdhkqmc";
 
-const gpt_func = (async (prompt) => {
+const gpt_func = (async (transcription,start_sequence, prompt_dict) => {
   const url = 'https://api.openai.com/v1/engines/davinci/completions';
-  const params = {
-    "prompt": prompt,
-    "max_tokens": 160,
-    "temperature": 0.7,
-    "frequency_penalty": 0.5
-  };
+  const params = prompt_dict
+  params["prompt"] += transcription + "\n###\n"+ start_sequence
   const headers = {
     'Authorization': 'Bearer ' + API_KEY,
   };
@@ -23,4 +19,13 @@ const gpt_func = (async (prompt) => {
   }
 });
 
+delete(key) {
+  if(this.hasKey(key)) {
+     delete this.container[key];
+     return true;
+  }
+  return false;
+};
+
 module.exports.gpt_func = gpt_func;
+
